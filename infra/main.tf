03017460388874993a777b7605d67ff96b97b50e
Uelinton <uelinton_reis@yahoo.com.br>
2026-03-04 11:23:17 -0300

    Correção do main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -83,7 +83,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   name                            = "vm-automation"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
-  size                            = "Standard_B1s"
+  size                            = "Standard_D2s_v3"
   admin_username                  = "azureuser"
   admin_password                  = var.admin_password
   disable_password_authentication = false
